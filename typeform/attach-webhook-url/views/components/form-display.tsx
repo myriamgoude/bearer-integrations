633c8b4975ddd841fce1311ddc4ea6a3b1e4ae87
Corentin Brossault <corentin.brossault@bearer.sh>
2019-02-28 18:49:49 +0100

    fix(ui): typo

--- typeform/attach-webhook-url/views/components/form-display.tsx
+++ typeform/attach-webhook-url/views/components/form-display.tsx
@@ -1,37 +1,37 @@
-import {Component, Prop} from '@bearer/core'
-import {Forms} from '../types'
-import ViewIcon from './icons/icon-view';
+import { Component, Prop } from '@bearer/core'
+import { Forms } from '../types'
+import ViewIcon from './icons/icon-view'
 
 @Component({
-    tag: 'form-display',
-    styleUrl: 'form-display.css',
-    shadow: true
+  tag: 'form-display',
+  styleUrl: 'form-display.css',
+  shadow: true
 })
 export class FormDisplay {
-    @Prop() items: Forms[];
+  @Prop() items: Forms[]
 
-    redirect = (form: Forms) => {
-        window.open(`${form._links.display}`, '_blank');
-    };
+  redirect = (form: Forms) => {
+    window.open(`${form._links.display}`, '_blank')
+  }
 
-    render() {
-        if (!this.items && !this.items.length) {
-            return null;
-        }
-
-        return (
-            this.items.map(form => {
-                return (
-                    <li class="list-item">
-                        <div>
-                            <p>Webhook URL successfully created for <strong>{form.title}</strong></p>
-                        </div>
-                        <a href={form._links.display} target="_blank" class="preview">
-                            <ViewIcon />
-                        </a>
-                    </li>
-                )
-            })
-        )
+  render() {
+    if (!this.items && !this.items.length) {
+      return null
     }
+
+    return this.items.map(form => {
+      return (
+        <li class='list-item'>
+          <div>
+            <p>
+              Webhook URL successfully attached to <strong>{form.title}</strong>
+            </p>
+          </div>
+          <a href={form._links.display} target='_blank' class='preview'>
+            <ViewIcon />
+          </a>
+        </li>
+      )
+    })
+  }
 }
