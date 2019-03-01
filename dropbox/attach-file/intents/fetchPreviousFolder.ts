22f1283a8f93a91e25ecb761208adabcfa2f9f09
Corentin Brossault <corentin.brossault@bearer.sh>
2019-03-01 15:55:44 +0100

    refactor: remove not in-use intent

---
+++ dropbox/attach-file/intents/fetchPreviousFolder.ts
@@ -0,0 +1,29 @@
+import { TOAUTH2AuthContext, FetchData, TFetchActionEvent, TFetchPromise } from '@bearer/intents'
+import Client from './client'
+import { File } from '../views/types'
+import { folders as query } from './queries'
+
+export default class FetchPreviousFolderIntent extends FetchData
+  implements FetchData<ReturnedData, any, TOAUTH2AuthContext> {
+  async action(event: TFetchActionEvent<Params, TOAUTH2AuthContext>): TFetchPromise<ReturnedData> {
+    const token = event.context.authAccess.accessToken
+    const folderPath = event.params.folderPath ? event.params.folderPath : ''
+    query.path = folderPath
+    delete query.include_mounted_folders
+    const { data } = await Client(token).post('/files/get_metadata', query)
+    if (data.errors) {
+      const message = data.errors.map((e: { message: string }) => e.message).join(', ')
+      return { error: message }
+    }
+    return { data }
+  }
+}
+
+/**
+ * Typing
+ */
+export type Params = {
+  folderPath: string
+}
+
+export type ReturnedData = File
