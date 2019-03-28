ef3003463b0eb1ab57cb2773aa20ee9ca7099528
Majuu <artur.maj@neoteric.eu>
2019-03-28 13:44:18 +0100

    Hubspot - Update contact complete

--- hubspot/create-entry/views/components/navigation-update.tsx
+++ hubspot/create-entry/views/components/navigation-update.tsx
@@ -63,14 +63,13 @@ export class NavigationUpdate {
                 <div class="content">
                     <div class="label">
                         {prop.same ? <div class="white-dot"></div> : <div class="green-dot"></div>}
-                        <label class="capital-letter">{prop.name}</label>
+                        <label class="capital-letter">{prop.property}</label>
                     </div>
                     {prop.same ? <span>{this.entry.properties[prop.name].value}</span> : <div class="values">
                         {this.entry.properties[prop.property] ? <span class="previous-value">{this.entry.properties[prop.property].value}</span> : null}
                         <input placeholder="Enter new value" class="value" value={prop.value} onInput={event => this.handleContactInput(event, prop.property)} />
                     </div>}
                 </div>
-                    // [prop.property]
             ))
         )
     }
